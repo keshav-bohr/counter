ae42f33a3b12f80ed3bad137a997a99f525d4783
Keshav Bohra <keshav.b@westagailelabs.com>
2018-10-29 22:00:34 +0530

    added the reducer

---
+++ src/container/App.js
@@ -0,0 +1,22 @@
+const reducer = (state = {
+    counter: 0
+}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+
+        default:
+            return state
+    }
+}
+
+export default reducer;
