912910ca9d9287ab4dd585b9a438876cf782c5a6
Keshav Bohra <keshav.b@westagailelabs.com>
2018-10-29 22:12:37 +0530

    added the container and made the connection

--- src/reducer/App.js
+++ src/reducer/App.js
@@ -14,6 +14,12 @@ const reducer = (state = {
                 counter: state.counter - 1
             }
 
+        case 'RESET':
+            return {
+                ...state,
+                counter: 0
+            }
+
         default:
             return state
     }
